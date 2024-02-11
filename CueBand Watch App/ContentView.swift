77b0ece3582acf23f31c88b263fad96420fa8083
MitchelMckee <79174382+MitchelMckee@users.noreply.github.com>
2024-02-11 16:14:00 +0000

    Initial Commit

---
+++ CueBand Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CueBand Watch App
+//
+//  Created by Mitchel Mckee on 11/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CueBand Watch App/CueBandApp.swift
@@ -0,0 +1,17 @@
+//
+//  CueBandApp.swift
+//  CueBand Watch App
+//
+//  Created by Mitchel Mckee on 11/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CueBand_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
